1de1c9fbea24841a07423b5f9e94dae3d7642561
AntonV0 <anton.vlasenko94@gmail.com>
2026-03-09 12:00:27 +0000

    feat: add user and html components

---
+++ FirstAngularProject/src/app/html/html.ts
@@ -0,0 +1,12 @@
+// This file defines an Angular component named 'Html' that serves as a template for displaying HTML content in the application. The component is decorated with the @Component decorator, which specifies the selector, template URL, and style URL for the component. The selector 'app-html' allows this component to be used in other templates by including the <app-html> tag. The templateUrl points to the HTML file that contains the structure of the component's view, while the styleUrl points to the CSS file that contains the styles for this component.
+
+import { Component } from '@angular/core';
+import { User } from '../user/user';
+
+@Component({
+  selector: 'app-html',
+  imports: [User],
+  templateUrl: './html.html',
+  styleUrl: './html.css',
+})
+export class Html {}
--- FirstAngularProject/src/app/user/user.ts
+++ FirstAngularProject/src/app/user/user.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {}
+export class User {
+  // A property to hold the name of the user, which can be used in the template for dynamic updates
+  name: string = 'User Component';
+
+  count = 0;
+  increment() {
+    this.count++;
+  }
+} 
