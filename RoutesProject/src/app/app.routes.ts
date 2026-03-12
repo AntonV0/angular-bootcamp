004771f14a5e9b75d7daccc1645b8f828016a692
AntonV0 <anton.vlasenko94@gmail.com>
2026-03-12 13:10:13 +0000

    feat: add routing and page components (main-menu, about-us, services, contact-us)

--- RoutesProject/src/app/app.routes.ts
+++ RoutesProject/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainMenu } from './main-menu/main-menu';
+import { AboutUs } from './about-us/about-us';
+import { Services } from './services/services';
+import { ContactUs } from './contact-us/contact-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: MainMenu },
+  {path: 'about-us', component: AboutUs },
+  {path: 'services', component: Services },
+  {path: 'contact-us', component: ContactUs },
+];
