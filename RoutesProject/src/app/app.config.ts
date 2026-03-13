60477f10f1b872d8f934215b0cb91f7157afb484
AntonV0 <anton.vlasenko94@gmail.com>
2026-03-13 14:39:43 +0000

    feat: update main menu navigation and app layout

--- RoutesProject/src/app/app.config.ts
+++ RoutesProject/src/app/app.config.ts
@@ -6,6 +6,6 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes) // This sets up the Angular router with the defined routes from app.routes.ts
   ]
 };
--- RoutesProject/src/app/main-menu/main-menu.ts
+++ RoutesProject/src/app/main-menu/main-menu.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { AddStudentComponent } from '../add-student.component';
+import { StudentListComponent } from '../student-list.component';
+import { PipesComponent } from '../pipes-component/pipes-component';
+import { SignalsComponent } from '../signals-component/signals-component';
+import { SignalsEffectComponent } from '../signals-effect-component/signals-effect-component';
+
 
 @Component({
   selector: 'app-main-menu',
-  imports: [],
+  imports: [AddStudentComponent, StudentListComponent, PipesComponent, SignalsComponent, SignalsEffectComponent],
   templateUrl: './main-menu.html',
   styleUrl: './main-menu.css',
 })
