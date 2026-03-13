bc2a45c52cf79f050896afd242620209f4e1d2ae
AntonV0 <anton.vlasenko94@gmail.com>
2026-03-13 14:37:43 +0000

    feat: add back button navigation to routed pages

--- RoutesProject/src/app/contact-us/contact-us.ts
+++ RoutesProject/src/app/contact-us/contact-us.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.css',
 })
